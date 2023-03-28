8dae5bca0ba157e7efa184d8a78de3819f7a2057
Siles <siles@Siless-MacBook-Pro.local>
2023-03-27 22:04:02 -0700

    Adding base screen class to build upon

---
+++ src/main/java/co/android/BaseScreen.java
@@ -0,0 +1,19 @@
+package co.android;
+
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebElement;
+
+// Step 5 - Create a base class to be used to inherit all common functionality
+public class BaseScreen {
+    private AndroidDriver driver;
+
+    public boolean isElementDisplayed(WebElement element) {
+        try {
+            element.isDisplayed();
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+}
